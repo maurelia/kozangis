2574e603cf5a5495e7fbc17f6bbb33724a05dc52
maurelia <jc.maurelia@gmail.com>
2023-12-07 01:16:04 -0300

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -1,19 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import leafmap
-from data.create_data import create_table
+import leafmap
 
 def app():
     st.title('Home')
 
     st.write("This is a sample home page in the mutliapp.")
     st.write("See `apps/home.py` to know how to use it.")
-    #leafmap.Map()
     st.markdown("### Sample Data")
-    df = create_table()
-    st.write(df)
 
+    m = leafmap.Map()
+    m
     st.write('Navigate to `Data Stats` page to visualize the data')
 
 
