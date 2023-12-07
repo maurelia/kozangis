4800e4912b8b9aa053a2b36653e872139973c01c
maurelia <jc.maurelia@gmail.com>
2023-12-07 11:39:59 -0300

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -21,12 +21,12 @@ if __name__ == "__main__":
     else:
         print("Unable to retrieve your GPS coordinates.")
 
-get_current_gps_coordinates()
 
 def app():
     st.title('Ubicación de las cámaras del Acueducto y Relaveducto KOZAN')
 
-    latitude
+    get_current_gps_coordinates()
+
     camaras = 'https://github.com/maurelia/kozangis/blob/edacf98a91f516740f0e16801fc58d82c5c3b4e2/data/CAMARAS.geojson'
 
     m = leafmap.Map(center=[-27.42581110601346, -70.26855756942263], zoom=13)
