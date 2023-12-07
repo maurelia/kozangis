7ba4d670bd57d3a3851d1847e0204d4fbb7776ea
maurelia <jc.maurelia@gmail.com>
2023-12-07 01:40:59 -0300

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -4,14 +4,13 @@ import pandas as pd
 import numpy as np
 
 def app():
-    st.title('Home')
+    st.title('Ubicación de las cámaras del Acueducto y Relaveducto KOZAN')
     m = leafmap.Map()
     m.add_basemap('ROADMAP')
-
+    m.to_streamlit()
     st.write("This is a sample home page in the mutliapp.")
     st.write("See `apps/home.py` to know how to use it.")
     st.markdown("### Sample Data")
-    m.to_streamlit()
     st.write('Navigate to `Data Stats` page to visualize the data')
 
 
