e0794668c83123ef4e8fa5e2c60f386ba2ebf57d
maurelia <jc.maurelia@gmail.com>
2023-12-07 12:02:07 -0300

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -32,7 +32,7 @@ def app():
     m = leafmap.Map(center=[-27.42581110601346, -70.26855756942263], zoom=13)
     m.add_basemap("HYBRID")
     m.add_geojson(camaras, layer_name="Camaras")
-    m.to_streamlit(width=700,height=500,add_layer_control=False)
+    m.to_streamlit(width=700,height=500,add_layer_control=True)
     
     st.write("Estas son las ubicaciones de las cámaras de registro del acueducto y relaveducto de Atacama Kozan")
     st.write("La aplicación le mostrará su ubicación y si hace click en la ubicación, le mostrara los datos mas relevantes a conocer")
