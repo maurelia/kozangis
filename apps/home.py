7f2e6524ba2de7948016d982e64f99491c727441
maurelia <jc.maurelia@gmail.com>
2023-12-07 01:17:36 -0300

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -5,12 +5,12 @@ import leafmap
 
 def app():
     st.title('Home')
+    m = leafmap.Map()
 
     st.write("This is a sample home page in the mutliapp.")
     st.write("See `apps/home.py` to know how to use it.")
     st.markdown("### Sample Data")
 
-    m = leafmap.Map()
     m
     st.write('Navigate to `Data Stats` page to visualize the data')
 
