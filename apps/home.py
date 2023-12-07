374acb14bd28df6e5fe7a915ded5e2d93b5d784b
maurelia <jc.maurelia@gmail.com>
2023-12-07 01:44:05 -0300

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -7,7 +7,7 @@ def app():
     st.title('Ubicación de las cámaras del Acueducto y Relaveducto KOZAN')
     m = leafmap.Map()
     m.add_basemap('ROADMAP')
-    m.to_streamlit()
+    m.to_streamlit(width=700,height=500)
     st.write("This is a sample home page in the mutliapp.")
     st.write("See `apps/home.py` to know how to use it.")
     st.markdown("### Sample Data")
