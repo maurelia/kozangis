f6c7a46d86d437f35a5c8717bf59a03aaa0c9b0c
maurelia <jc.maurelia@gmail.com>
2023-12-07 10:01:33 -0300

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -2,9 +2,32 @@ import leafmap.foliumap as leafmap
 import streamlit as st
 import pandas as pd
 import numpy as np
+import geocoder
+
+def get_current_gps_coordinates():
+    g = geocoder.ip('me')#this function is used to find the current information using our IP Add
+    if g.latlng is not None: #g.latlng tells if the coordiates are found or not
+        return g.latlng
+    else:
+        return None
+
+if __name__ == "__main__":
+    coordinates = get_current_gps_coordinates()
+    if coordinates is not None:
+        latitude, longitude = coordinates
+        print(f"Your current GPS coordinates are:")
+        print(f"Latitude: {latitude}")
+        print(f"Longitude: {longitude}")
+    else:
+        print("Unable to retrieve your GPS coordinates.")
+
+
 
 def app():
     st.title('Ubicación de las cámaras del Acueducto y Relaveducto KOZAN')
+
+
+
     
     camaras = 'https://github.com/maurelia/kozangis/blob/edacf98a91f516740f0e16801fc58d82c5c3b4e2/data/CAMARAS.geojson'
 
