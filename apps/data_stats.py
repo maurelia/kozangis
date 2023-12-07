2c54d3b1628f20c04fa6437de9cf3fa1f2cf9c49
maurelia <jc.maurelia@gmail.com>
2023-12-07 01:39:02 -0300

    Update data_stats.py

--- apps/data_stats.py
+++ apps/data_stats.py
@@ -1,10 +1,7 @@
-import folium
+import leafmap
 import streamlit as st
 import numpy as np
 import pandas as pd
-import leafmap
-
-from data.create_data import create_table
 
 def app():
     st.title('Data Stats')
@@ -13,10 +10,3 @@ def app():
     st.write("See `apps/data_stats.py` to know how to use it.")
 
     st.markdown("### Plot Data")
-    m = leafmap.Map()
-    m.add_tile_layer(
-    url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
-    name="Google Satellite",
-    attribution="Google",
-    )
-    m
