acbcfb158e058da2e785bdf878b0e0fa590e213d
maurelia <jc.maurelia@gmail.com>
2023-12-07 01:08:06 -0300

    Update data_stats.py

--- apps/data_stats.py
+++ apps/data_stats.py
@@ -3,7 +3,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from streamlit_folium import st_folium
-from folium.plugins import LocateControl
 
 from data.create_data import create_table
 
@@ -13,7 +12,8 @@ def app():
     st.write("This is a sample data stats in the mutliapp.")
     st.write("See `apps/data_stats.py` to know how to use it.")
     m = folium.Map(location=[-27.378629205322742, -70.25008507260203], zoom_start=16)
-    LocateControl().Add_to(m)
+    folium.plugins.LocateControl().add_to(m)
+
 
     st.markdown("### Plot Data")
     st_data = st_folium(m, width=725)
