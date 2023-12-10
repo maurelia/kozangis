c8c21af007764b89d67a4cb42886cd6958d8411e
maurelia <jc.maurelia@gmail.com>
2023-12-09 22:30:25 -0300

    Update tercerdren.py

--- apps/tercerdren.py
+++ apps/tercerdren.py
@@ -1,1 +1,12 @@
+import leafmap
+import streamlit as st
+import numpy as np
+import pandas as pd
 
+def app():
+    st.title('Data Stats')
+
+    st.write("This is a sample data stats in the mutliapp.")
+    st.write("See `apps/data_stats.py` to know how to use it.")
+
+    st.markdown("### Plot Data")
