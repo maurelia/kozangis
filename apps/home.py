617c09b1003b6e537dccb6c678dcd4a7d2276878
maurelia <jc.maurelia@gmail.com>
2023-12-10 00:27:53 -0300

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -4,27 +4,8 @@ import pandas as pd
 import numpy as np
 import geocoder
 
-def get_current_gps_coordinates():
-    g = geocoder.ip('me')#this function is used to find the current information using our IP Add
-    if g.latlng is not None: #g.latlng tells if the coordiates are found or not
-        return g.latlng
-    else:
-        return None
-
-    coordinates = get_current_gps_coordinates()
-    if coordinates is not None:
-        latitude, longitude = coordinates
-        print(f"Your current GPS coordinates are:")
-        print(f"Latitude: {latitude}")
-        print(f"Longitude: {longitude}")
-    else:
-        print("Unable to retrieve your GPS coordinates.")
-
 def app():
     st.title('Ubicación de las cámaras del Acueducto y Relaveducto KOZAN')
-
-    location=get_current_gps_coordinates()
-
 
     camaras = 'https://github.com/maurelia/kozangis/blob/e0794668c83123ef4e8fa5e2c60f386ba2ebf57d/data/CAMARAS.geojson'
 
