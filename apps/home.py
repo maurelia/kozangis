2da207739895b94dc0ed17b03005ce7d1ff9f339
maurelia <jc.maurelia@gmail.com>
2023-12-10 01:11:27 -0300

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -18,7 +18,7 @@ def app():
     m = leafmap.Map(center=[latitude, longitude], zoom=13, draw_control=False, measure_control=False, fullscreen_control=True, attribution_control=False)
     m.add_basemap("HYBRID")
     m.add_geojson(camaras, layer_name="Camaras")
-    m.add_marker([latitude, longitude], popup=None, tooltip=None, icon=folium.Icon(color='red',icon_color='white',icon='ban-circle',), draggable=False)     
+    m.add_marker([latitude, longitude], popup=None, tooltip=None, icon=None, draggable=False)     
     m.to_streamlit(width=600,height=700,add_layer_control=True)
     
     st.write(f"{latitude},{longitude}")
