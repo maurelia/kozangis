2ec833615cc4ee44e2f4664be7be373975298628
maurelia <jc.maurelia@gmail.com>
2023-12-09 22:39:48 -0300

    Update tercerdren.py

--- apps/tercerdren.py
+++ apps/tercerdren.py
@@ -4,9 +4,12 @@ import numpy as np
 import pandas as pd
 
 def app():
-    st.title('Data Stats')
+    st.title('Visor de la construcción del Tercer Dren')
 
-    st.write("This is a sample data stats in the mutliapp.")
-    st.write("See `apps/data_stats.py` to know how to use it.")
-
-    st.markdown("### Plot Data")
+    m = leafmap.Map(center=[-27.42581110601346, -70.26855756942263], zoom=13)
+    m.add_basemap("HYBRID")
+    m.add_geojson(camaras, layer_name="Camaras")
+    m.to_streamlit(width=700,height=500,add_layer_control=True)
+    
+    st.write("Estas son las ubicaciones de las cámaras de registro del acueducto y relaveducto de Atacama Kozan")
+    st.write("La aplicación le mostrará su ubicación y si hace click en la ubicación, le mostrara los datos mas relevantes a conocer")
